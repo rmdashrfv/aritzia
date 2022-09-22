e43bbeb06c5d5d0d7cb3e33cdf0088b2aa5efccc
Michael Law <rmdashrfv@hey.com>
2022-09-22 17:29:37 -0400

    Added fetch request for cars

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  // const [stateVariable, functionToUpdateTheVariable] = useState(initialStateValue)
+  const [cars, setCars] = useState([])
+
+  useEffect(() => {
+    const request = async () => {
+      let req = await fetch("https://raw.githubusercontent.com/rmdashrfv/aritzia/main/MOCK_DATA.json")
+      let res = await req.json()
+      console.log(res)
+    }
+    request()
+  }, [])
+   
+
   return (
     <div>
       <nav id="navbar">
@@ -20,6 +34,14 @@ function App() {
         <div id="left-panel"></div>
         <div id="right-panel"></div>
       </div>
+      <h2>Aritzia Cars</h2>
+      {
+        cars.map((car) => {
+          return(
+            <div>{car.model}</div>
+          )
+        })
+      }
     </div>
   );
 }
