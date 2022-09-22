3e8ea868dae07a00119483c135d2907ad3767c12
Michael Law <rmdashrfv@hey.com>
2022-09-22 16:49:03 -0400

    setting up navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav id="navbar">
+        <div id="notification-bar">
+          <p>Free shipping & returns on all orders, all the time. Shop now</p>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </nav>
+      <h1>Aritzia</h1>
     </div>
   );
 }
